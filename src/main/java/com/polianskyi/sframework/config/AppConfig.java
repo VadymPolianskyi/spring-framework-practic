e8a3e25cfe840689eeb669b6e238b2487695849c
vadym <vadym.polyanski@gmail.com>
2018-01-14 22:17:53 +0200

    created mvc configs

---
+++ src/main/java/com/polianskyi/sframework/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.polianskyi.sframework.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.polianskyi.sframework")
+public class AppConfig {
+
+}
---
+++ src/main/java/com/polianskyi/sframework/config/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package com.polianskyi.sframework.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+}
