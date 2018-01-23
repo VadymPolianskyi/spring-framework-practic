5c98ccef04cebb3b5f1e6eade969ebfbd5fa41fc
vadym <vadym.polyanski@gmail.com>
2018-01-23 08:20:07 +0200

    added headers mapping

--- src/main/java/com/polianskyi/sframework/controller/AppController.java
+++ src/main/java/com/polianskyi/sframework/controller/AppController.java
@@ -2,10 +2,7 @@ package com.polianskyi.sframework.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/app")
@@ -16,5 +13,11 @@ public class AppController {
     public String setupForm(@RequestParam int appId, ModelMap model) {
         model.addAttribute("appAttr", String.valueOf(appId));
         return "appForm";
+    }
+
+    @RequestMapping("/displayHeadelInfo.do")
+    public void displayHeadelInfo(@RequestHeader("Accept-Encoding") String encoding,
+                                  @RequestHeader("Keep-Alive") long keepAlive) {
+        
     }
 }
